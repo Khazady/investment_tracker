e5da5d6e499b1e40210941d5480aee3ff0e51f1c
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2025-06-09 15:07:24 +0200

    add User mongo model

---
+++ models/User.ts
@@ -0,0 +1,79 @@
+import { hashPassword, verifyPassword } from "@/lib/utils";
+import bcrypt from "bcryptjs";
+import mongoose, { Model, Schema } from "mongoose";
+
+export interface IUser extends Document {
+  username: string;
+  email: string;
+  passwordHash: string;
+  avatarUrl?: string | null;
+  publicSlug: string;
+  bio?: string | null;
+  createdAt: Date;
+  comparePassword(candidate: string): Promise<boolean>;
+}
+
+const UserSchema = new Schema<IUser>(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^.+@.+\..+$/,
+      lowercase: true,
+      trim: true,
+    },
+    passwordHash: {
+      type: String,
+      required: true,
+    },
+    avatarUrl: {
+      type: String,
+      default: null,
+    },
+    publicSlug: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    bio: {
+      type: String,
+      default: null,
+    },
+    createdAt: {
+      type: Date,
+      required: true,
+      default: () => new Date(),
+    },
+  },
+  {
+    versionKey: false,
+  },
+);
+
+// Hash password before saving
+UserSchema.pre<IUser>("save", async function (next) {
+  if (!this.isModified("passwordHash")) return next();
+  try {
+    this.passwordHash = await hashPassword(this.passwordHash);
+    next();
+  } catch (err) {
+    next(err as Error);
+  }
+});
+// Instance method to compare passwords
+UserSchema.methods.comparePassword = function (candidate: string) {
+  // возвращает Promise<boolean>
+  return verifyPassword(candidate, this.passwordHash);
+};
+
+const User: Model<IUser> =
+  mongoose.models.User || mongoose.model<IUser>("User", UserSchema);
+
+export default User;
