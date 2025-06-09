7645c854c811ecb3606a7b3f02d98eb23b557bf7
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2025-06-09 17:05:37 +0200

    refactor SignInPage to use Input and Typography components for improved structure

--- app/(public)/auth/signin/page.tsx
+++ app/(public)/auth/signin/page.tsx
@@ -1,18 +1,14 @@
-import styles from './page.module.css';
+import { Input } from "@/components/ui/Input/Input";
+import { Typography } from "@/components/ui/Typography/Typography";
+import styles from "./page.module.css";
 
 export default function SignInPage() {
   return (
     <div className={styles.wrapper}>
       <form className={styles.form} action="#" method="post">
-        <h1>Sign In</h1>
-        <label>
-          Email
-          <input type="email" name="email" required />
-        </label>
-        <label>
-          Password
-          <input type="password" name="password" required />
-        </label>
+        <Typography variant="h1">Sign In</Typography>
+        <Input label="Email" type="email" name="email" required />
+        <Input label="Password" type="password" name="password" required />
         <button type="submit">Log in</button>
       </form>
     </div>
