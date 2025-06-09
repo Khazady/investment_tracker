0c4b5ea3aef7c9c384509c4034c0412fba00dc79
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2025-06-09 15:07:08 +0200

    add getUser db-query

---
+++ lib/db-queries/user.ts
@@ -0,0 +1,10 @@
+import User, { IUser } from "@/models/User";
+
+export async function getUser(filter: Partial<IUser>) {
+  try {
+    return await User.findOne({ filter });
+  } catch (error) {
+    console.error("Failed to fetch user:", error);
+    throw new Error("Failed to fetch user.");
+  }
+}
