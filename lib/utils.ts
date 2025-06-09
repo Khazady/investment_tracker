7bbb69c71641922c64f1335e38e287d9bf4d9770
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2025-06-09 19:47:36 +0200

    remove bcryptjs dependency and implement password hashing and verification using Node.js crypto module

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,17 +1,23 @@
-import { compare, hash } from "bcryptjs";
+import crypto from "node:crypto";
 
+export function hashPassword(password: string) {
+  const salt = crypto.randomBytes(16).toString("hex");
+
+  const hashedPassword = crypto.scryptSync(password, salt, 64);
+  return hashedPassword.toString("hex") + ":" + salt;
+}
+
+export function verifyPassword(
+  storedPassword: string,
+  suppliedPassword: string,
+) {
+  const [hashedPassword, salt] = storedPassword.split(":");
+  const hashedPasswordBuf = Buffer.from(hashedPassword, "hex");
+  const suppliedPasswordBuf = crypto.scryptSync(suppliedPassword, salt, 64);
+  return crypto.timingSafeEqual(hashedPasswordBuf, suppliedPasswordBuf);
+}
 export function cn(...classes: (string | false | undefined | null)[]) {
   return classes.filter(Boolean).join(" ");
-}
-
-export async function hashPassword(password: string) {
-  const hashedPassword = await hash(password, 12);
-  return hashedPassword;
-}
-
-export async function verifyPassword(password: string, hashedPassword: string) {
-  const isValid = await compare(password, hashedPassword);
-  return isValid;
 }
 
 export function debounce(func: Function, delay: number) {
