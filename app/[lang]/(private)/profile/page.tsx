66f946e6147cb9368543aedbd566ea206e3967a2
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2025-08-25 16:25:26 +0200

    fix typo

--- app/[lang]/(private)/profile/page.tsx
+++ app/[lang]/(private)/profile/page.tsx
@@ -1,8 +1,8 @@
+import PasswordForm from "@/app/[lang]/(private)/profile/_components/PasswordForm/PasswordForm";
 import Typography from "@/components/ui/Typography/Typography";
 import { getDictionary } from "@/lib/dictionaries/server";
 import { getCurrentUser } from "@/lib/server/getCurrentUser";
 import type { PageProps } from "@/lib/types/app";
-import PasswordForm from "./_components/PassowrdForm/PasswordForm";
 import ProfileForm from "./_components/ProfileForm/ProfileForm";
 
 // SSR: reads cookies and queries MongoDB on each request
