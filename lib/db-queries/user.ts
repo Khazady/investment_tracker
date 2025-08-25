3edc5a05ff7f655055bf6f3b48a10e1696f7c273
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2025-08-25 17:26:54 +0200

    fix typescript user.ts

--- lib/db-queries/user.ts
+++ lib/db-queries/user.ts
@@ -1,8 +1,9 @@
 import { ERRORS } from "@/lib/constants/errors";
 import type { IUser } from "@/models/User";
 import User from "@/models/User";
+import type { FilterQuery } from "mongoose";
 
-export async function getUser(filter: Partial<IUser>) {
+export async function getUser(filter: FilterQuery<IUser>) {
   try {
     return await User.findOne(filter);
   } catch (error) {
